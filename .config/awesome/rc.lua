fa45938dc771c7bfd7bd13c87d3a29e9301805aa
Göktuğ Korkmaz <goktugkorkmaz@gmail.com>
2018-11-27 01:20:38 +0300

    added awesome configs

---
+++ .config/awesome/rc.lua
@@ -0,0 +1,283 @@
+
+local gears         = require("gears")
+local awful         = require("awful")
+                      require("awful.autofocus")
+local wibox         = require("wibox")
+local beautiful     = require("beautiful")
+local naughty       = require("naughty")
+local lain          = require("lain")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
+                      require("awful.hotkeys_popup.keys")
+local keys = require("keys")
+local variables = require("variables")
+
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.connect_signal("debug::error", function (err)
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
+-- }}}
+
+
+awful.util.terminal = terminal
+awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.magnifier,
+    awful.layout.suit.corner.nw,
+    awful.layout.suit.corner.ne,
+    awful.layout.suit.corner.sw,
+    awful.layout.suit.corner.se,
+    lain.layout.cascade,
+    lain.layout.cascade.tile,
+    lain.layout.centerwork,
+    lain.layout.centerwork.horizontal,
+    lain.layout.termfair,
+    lain.layout.termfair.center,
+}
+
+awful.util.tagnames = {
+	{
+		{name = "1", sel = true},
+		{name = "2", gap = 0},
+		{name = "3", lay = awful.layout.layouts[3], mw = 0.87},
+		{name = "4"},
+		{name = "5"},
+		{name = "6"},
+		{name = "7"},
+		{name = "8"},
+		{name = "9"},
+		{name = "10", lay = awful.layout.layouts[5]},
+	},
+	{
+		{name = "1", sel = true},
+		{name = "2"},
+		{name = "3"},
+		{name = "4"},
+		{name = "5"},
+		{name = "6"},
+		{name = "7"},
+		{name = "8"},
+		{name = "9"},
+		{name = "10"},
+	}
+}
+
+awful.util.taglist_buttons = gears.table.join(
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3, function(t)
+        if client.focus then
+            client.focus:toggle_tag(t)
+        end
+    end),
+    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+)
+
+awful.util.tasklist_buttons = gears.table.join(
+    awful.button({ }, 1, function (c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            --c:emit_signal("request::activate", "tasklist", {raise = true})<Paste>
+
+            -- Without this, the following
+            -- :isvisible() makes no sense
+            c.minimized = false
+            if not c:isvisible() and c.first_tag then
+                c.first_tag:view_only()
+            end
+            -- This will also un-minimize
+            -- the client, if needed
+            client.focus = c
+            c:raise()
+        end
+    end),
+    awful.button({ }, 2, function (c) c:kill() end),
+    awful.button({ }, 3, function ()
+        local instance = nil
+
+        return function ()
+            if instance and instance.wibox.visible then
+                instance:hide()
+                instance = nil
+            else
+                instance = awful.menu.clients({theme = {width = 250}})
+            end
+        end
+    end),
+    awful.button({ }, 4, function () awful.client.focus.byidx(1) end),
+    awful.button({ }, 5, function () awful.client.focus.byidx(-1) end)
+)
+
+lain.layout.termfair.nmaster           = 3
+lain.layout.termfair.ncol              = 1
+lain.layout.termfair.center.nmaster    = 3
+lain.layout.termfair.center.ncol       = 1
+lain.layout.cascade.tile.offset_x      = 2
+lain.layout.cascade.tile.offset_y      = 32
+lain.layout.cascade.tile.extra_padding = 5
+lain.layout.cascade.tile.nmaster       = 5
+lain.layout.cascade.tile.ncol          = 2
+
+beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "dremora"))
+
+-- }}}
+
+-- {{{ Menu
+local myawesomemenu = {
+    { "hotkeys", function() return false, hotkeys_popup.show_help end },
+    { "manual", terminal .. " -e man awesome" },
+    { "edit config", string.format("%s -e %s %s", terminal, editor, awesome.conffile) },
+    { "restart", awesome.restart },
+    { "quit", function() awesome.quit() end }
+}
+--menubar.utils.terminal = terminal -- Set the Menubar terminal for applications that require it
+-- }}}
+
+-- {{{ Screen
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", function(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, true)
+    end
+end)
+-- Create a wibox for each screen and add it
+awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) end)
+-- }}}
+
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = awful.client.focus.filter,
+                     raise = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons,
+                     screen = awful.screen.preferred,
+                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+     }
+    },
+
+    -- Floating clients.
+    { rule_any = {
+        instance = {
+          "DTA",  -- Firefox addon DownThemAll.
+          "copyq",  -- Includes session name in class.
+        },
+        class = {
+          "Arandr",
+          "Gpick",
+          "Kruler",
+          "MessageWin",  -- kalarm.
+          "Sxiv",
+          "Wpa_gui",
+          "pinentry",
+          "veromix",
+          "xtightvncviewer"},
+
+        name = {
+          "Event Tester",  -- xev.
+        },
+        role = {
+          "AlarmWindow",  -- Thunderbird's calendar.
+          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+        }
+      }, properties = { floating = true }},
+
+    -- Add titlebars to normal clients and dialogs
+    { rule_any = {type = { "normal", "dialog" }
+      }, properties = { titlebars_enabled = false }
+    },
+
+    -- Set Firefox to always map on the tag named "2" on screen 1.
+    -- { rule = { class = "Firefox" },
+    --   properties = { screen = 1, tag = "2" } },
+}
+
+-- }}}
+
+-- {{{ Signals
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function (c)
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    -- if not awesome.startup then awful.client.setslave(c) end
+
+    if awesome.startup and
+      not c.size_hints.user_position
+      and not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
+end)
+
+-- Add a titlebar if titlebars_enabled is set to true in the rules.
+
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise = true})
+end)
+
+-- No border for maximized clients
+function border_adjust(c)
+    if c.maximized then -- no borders if only 1 client visible
+        c.border_width = 0
+    elseif #awful.screen.focused().clients > 1 then
+        c.border_width = beautiful.border_width
+        c.border_color = beautiful.border_focus
+    end
+end
+
+for _,i in pairs(autostart) do
+	awful.spawn.with_shell("~/.config/awesome/autorun.sh " .. i)
+end
+
+quake = lain.util.quake {
+	app = terminal,
+	horiz = "center",
+	height = 0.4,
+	width = 1
+}
+
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
---
+++ .config/awesome/themes/dremora/theme.lua
@@ -0,0 +1,280 @@
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+
+local os = os
+
+local theme                                     = {}
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/dremora"
+theme.wallpaper                                 = os.getenv("HOME") .. "/.config/wall.png"
+theme.font                                      = "Roboto Medium 9"
+theme.taglist_font                              = "Roboto Bold 9"
+theme.iconFont      							= "Font Awesome 5 Free Regular 9"
+theme.fg_normal                                 = "#bbb"
+theme.fg_focus                                  = "#DDDCFF"
+theme.bg_normal                                 = "#2F343F"
+theme.bg_focus                                  = "#2F343F"
+theme.fg_urgent                                 = "#CC9393"
+theme.bg_urgent                                 = "#2A1F1E"
+theme.border_width                              = "1"
+theme.border_normal                             = "#121212"
+theme.border_focus                              = "#8e1eff"
+theme.titlebar_bg_focus                         = "#292929"
+theme.taglist_fg_focus                          = "#dddcff"
+theme.taglist_fg_normal                         = "#dddcff"
+theme.taglist_bg_focus                          = "#444"
+theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
+theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
+theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
+theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
+theme.layout_tile                               = theme.dir .. "/icons/tile.png"
+theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.dir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.tasklist_align							= "center"
+theme.useless_gap                               = 5
+theme.systray_icon_spacing					    = 3
+theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+
+
+local markup     = lain.util.markup
+local separators = lain.util.separators
+local white      = theme.fg_focus
+local gray       = "#858585"
+
+-- Textclock
+local cal_icon = "  <span font=\"".. theme.iconFont .."\"></span> "
+local mytextclock = wibox.widget.textclock(markup(white, cal_icon) .. markup(white, " %d ")
+.. markup(gray, "%b ") .. markup(white, "%Y ") .. markup(gray, " %a ") .. markup(white, "%H:%M %p "))
+mytextclock.font = theme.font
+
+-- Calendar
+theme.cal = lain.widget.cal({
+    attach_to = { mytextclock },
+    notification_preset = {
+        font = "Misc Tamsyn 11",
+        fg   = white,
+        bg   = theme.bg_normal
+}})
+mytextclock:disconnect_signal("mouse::enter", theme.cal.hover_on)
+
+-- MPD
+theme.mpd = lain.widget.mpd({
+    settings = function()
+        mpd_notification_preset.fg = white
+        artist = " " .. mpd_now.artist .. " "
+        title  = " " .. mpd_now.title  .. " "
+
+        if mpd_now.state == "pause" then
+            artist = "mpd "
+            title  = "paused "
+        elseif mpd_now.state == "stop" then
+            artist = ""
+            title  = ""
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+    end
+})
+
+-- CPU
+theme.cpu = lain.widget.cpu({
+	settings = function()
+		local cpu_icon = "  <span font=\"".. theme.iconFont .."\"></span> "
+		widget:set_markup(markup.font(theme.font, markup(white, cpu_icon) .. markup(white, cpu_now.usage)))
+	end
+})
+
+-- Temp
+theme.temp = lain.widget.temp({
+	tempfile = "/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp1_input",
+	settings = function()
+		local temp_icon = "  <span font=\"".. theme.iconFont .."\"></span> "
+		local cur_temp = coretemp_now .. "c "
+		widget:set_markup(markup.font(theme.font, markup(temp_col, temp_icon) .. markup(temp_col, cur_temp)))
+	end
+})
+
+-- Battery
+theme.bat = lain.widget.bat({
+	notify = "off",
+	timeout = 10,
+    settings = function()
+		icon_color = white
+		if (bat_now.perc == 100) then
+			bat_icon = ""
+		elseif (bat_now.perc >= 60 and bat_now.perc < 100) then
+			bat_icon = ""
+		elseif (bat_now.perc >= 20 and bat_now.perc < 60) then
+			bat_icon = ""
+		elseif (bat_now.perc > 20) then
+			bat_icon = ""
+			icon_color = "#ff1e8e"
+		end
+        bat_header = "  <span font=\"".. theme.iconFont .."\">" .. bat_icon .. "</span> "
+		if (bat_now.status == "Charging" or bat_now.status == "Full") then
+			bat_color = "#1e8eff"
+		elseif (bat_now.status == "Discharging" and bat_now.perc <= 15) then
+			bat_color = "#ff1e8e"
+		else
+			bat_color = white
+		end
+        bat_p      = bat_now.perc .. "% "
+        widget:set_markup(markup.font(theme.font, markup(icon_color, bat_header) .. markup(bat_color, bat_p)))
+    end
+})
+
+theme.systray = wibox.widget.systray()
+theme.systray:set_base_size(15)
+
+-- Volume
+theme.volume = lain.widget.pulse {
+    settings = function()
+		cur_vol = tonumber(volume_now.left)
+		vol_color = white
+		if (cur_vol > 80 and volume_now.muted == "no") then
+			vol_icon = ""
+		elseif (cur_vol > 50 and volume_now.muted == "no") then
+			vol_icon = ""
+		elseif (cur_vol > 20 and volume_now.muted == "no") then
+			vol_icon = ""
+		else
+			vol_icon = ""
+			vol_color = "#ff1e8e"
+		end
+		vheader = "  <span font=\"".. theme.iconFont .."\">" .. vol_icon .. "</span> "
+        vlevel = volume_now.left ..  "% "
+        if volume_now.muted == "yes" then
+            vlevel = "M "
+        end
+        widget:set_markup(markup.font(theme.font, markup(vol_color, vheader) .. markup(vol_color, vlevel)))
+	end
+}
+theme.volume.widget:buttons(awful.util.table.join(
+    awful.button({}, 1, function() -- left click
+        awful.spawn("pavucontrol")
+    end),
+    awful.button({}, 2, function() -- middle click
+        os.execute(string.format("pactl set-sink-volume %d 100%%", theme.volume.device))
+        theme.volume.update()
+    end),
+    awful.button({}, 3, function() -- right click
+        os.execute(string.format("pactl set-sink-mute %d toggle", theme.volume.device))
+        theme.volume.update()
+    end),
+    awful.button({}, 4, function() -- scroll up
+        os.execute(string.format("pactl set-sink-volume %d +1%%", theme.volume.device))
+        theme.volume.update()
+    end),
+    awful.button({}, 5, function() -- scroll down
+        os.execute(string.format("pactl set-sink-volume %d -1%%", theme.volume.device))
+        theme.volume.update()
+    end)
+))
+
+-- Weather
+theme.weather = lain.widget.weather({
+    city_id = 2643743, -- placeholder (London)
+    notification_preset = { fg = white }
+})
+
+-- Separators
+local first     = wibox.widget.textbox('<span font="Misc Tamsyn 4"> </span>')
+local arrl_pre  = separators.arrow_right("alpha", "#1A1A1A")
+local arrl_post = separators.arrow_right("#1A1A1A", "alpha")
+
+function theme.at_screen_connect(s)
+
+    -- If wallpaper is a function, call it with the screen
+    local wallpaper = theme.wallpaper
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+
+    -- Tags
+    --awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
+	for _, i in pairs(awful.util.tagnames[s.index]) do
+		awful.tag.add(i.name, {
+			layout = i.lay or awful.layout.layouts[1],
+			gap = i.gap or theme.useless_gap,
+			screen = s,
+			selected = i.sel or false,
+			master_width_factor = i.mw or 0.5,
+		})
+	end
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+                           awful.button({}, 1, function () awful.layout.inc( 1) end),
+                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
+                           awful.button({}, 3, function () awful.layout.inc(-1) end),
+                           awful.button({}, 4, function () awful.layout.inc( 1) end),
+                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.focused, awful.util.tasklist_buttons)
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 18, bg = theme.bg_normal, fg = theme.fg_normal })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            s.mylayoutbox,
+            first,
+            s.mytaglist,
+            s.mypromptbox,
+            first,
+            theme.mpd.widget,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            first,
+			theme.cpu.widget,
+            theme.bat.widget,
+            theme.volume.widget,
+            mytextclock,
+            theme.systray,
+        },
+    }
+end
+
+return theme
