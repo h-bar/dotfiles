0a76ff170ccf48f4005dfb22fc3c621945e428a2
Göktuğ Korkmaz <goktugkorkmaz@gmail.com>
2019-02-06 01:36:31 +0300

    beutification

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,12 +1,12 @@
-local gears         = require("gears")
-local awful         = require("awful")
-                      require("awful.autofocus")
-local wibox         = require("wibox")
-local beautiful     = require("beautiful")
-local naughty       = require("naughty")
-local lain          = require("lain")
+local gears = require("gears")
+local awful = require("awful")
+require("awful.autofocus")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+local naughty = require("naughty")
+local lain = require("lain")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
-                      require("awful.hotkeys_popup.keys")
+require("awful.hotkeys_popup.keys")
 local keys = require("keys")
 local variables = require("variables")
 
@@ -15,8 +15,8 @@ local variables = require("variables")
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+        title = "Oops, there were errors during startup!",
+    text = awesome.startup_errors })
 end
 
 -- Handle runtime errors after startup
@@ -27,8 +27,8 @@ do
         in_error = true
 
         naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
+            title = "Oops, an error happened!",
+        text = tostring(err) })
         in_error = false
     end)
 end
@@ -44,15 +44,15 @@ awful.util.taglist_buttons = gears.table.join(
     awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
 )
 
-lain.layout.termfair.nmaster           = 3
-lain.layout.termfair.ncol              = 1
-lain.layout.termfair.center.nmaster    = 3
-lain.layout.termfair.center.ncol       = 1
-lain.layout.cascade.tile.offset_x      = 2
-lain.layout.cascade.tile.offset_y      = 32
+lain.layout.termfair.nmaster = 3
+lain.layout.termfair.ncol = 1
+lain.layout.termfair.center.nmaster = 3
+lain.layout.termfair.center.ncol = 1
+lain.layout.cascade.tile.offset_x = 2
+lain.layout.cascade.tile.offset_y = 32
 lain.layout.cascade.tile.extra_padding = 5
-lain.layout.cascade.tile.nmaster       = 5
-lain.layout.cascade.tile.ncol          = 2
+lain.layout.cascade.tile.nmaster = 5
+lain.layout.cascade.tile.ncol = 2
 
 beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "dremora"))
 
@@ -80,66 +80,66 @@ awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) 
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-					 focus = awful.client.focus.filter,
-                     keys = clientkeys,
-					 raise = false,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen,
-					 size_hints_honor = false,
-					 honor_padding = true,
-					 honor_workarea = true,
-					 minimized = false
-     }
+        properties = { border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            keys = clientkeys,
+            raise = false,
+            buttons = clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+            size_hints_honor = false,
+            honor_padding = true,
+            honor_workarea = true,
+            minimized = false
+        }
     },
 
     -- Floating clients.
     { rule_any = {
         instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-        },
-        class = {
-          "Arandr",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Wpa_gui",
-          "pinentry",
-          "veromix",
-          "xtightvncviewer"},
-
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
-	  { rule = { class = "Steam" }, properties = { screen = 1, tag = "10", }},
-	  {
-		  rule = { class = "Steam" },
-		  except = { name = "Steam" },
-	   	  properties = { ontop = true, }
-	  },
-
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = false }
+        "DTA", -- Firefox addon DownThemAll.
+        "copyq", -- Includes session name in class.
     },
-	{ rule_any = {
-		class = {"file_progress",},
-		type = { "dialog" },
-		}, properties = { ontop = true,},
-	}
+    class = {
+        "Arandr",
+        "Gpick",
+        "Kruler",
+        "MessageWin", -- kalarm.
+        "Sxiv",
+        "Wpa_gui",
+        "pinentry",
+        "veromix",
+    "xtightvncviewer"},
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    name = {
+        "Event Tester", -- xev.
+    },
+    role = {
+        "AlarmWindow", -- Thunderbird's calendar.
+        "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+    }
+}, properties = { floating = true }},
+{ rule = { class = "Steam" }, properties = { screen = 1, tag = "10", }},
+{
+    rule = { class = "Steam" },
+    except = { name = "Steam" },
+    properties = { ontop = true, }
+},
+
+-- Add titlebars to normal clients and dialogs
+{ rule_any = {type = { "normal", "dialog" }
+    }, properties = { titlebars_enabled = false }
+},
+{ rule_any = {
+    class = {"file_progress", },
+    type = { "dialog" },
+}, properties = { ontop = true, },
+}
+
+-- Set Firefox to always map on the tag named "2" on screen 1.
+-- { rule = { class = "Firefox" },
+--   properties = { screen = 1, tag = "2" } },
 }
 
 -- }}}
@@ -147,56 +147,56 @@ awful.rules.rules = {
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+-- Set the windows at the slave,
+-- i.e. put it at the end of others instead of setting it master.
+-- if not awesome.startup then awful.client.setslave(c) end
 
-	if not awesome.startup then awful.client.setslave(c) end
+if not awesome.startup then awful.client.setslave(c) end
 
-    if awesome.startup and
-      not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c, {honor_padding = true})
-    end
+if awesome.startup and
+not c.size_hints.user_position
+and not c.size_hints.program_position then
+-- Prevent clients from being unreachable after screen count changes.
+awful.placement.no_offscreen(c, {honor_padding = true})
+end
 end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = true})
+c:emit_signal("request::activate", "mouse_enter", {raise = true})
 end)
 
 client.connect_signal("property::minimized", function(c)
-	c.minimized = false
+c.minimized = false
 end)
 
 -- No border for maximized clients
 function border_adjust(c)
-    if c.maximized then -- no borders if only 1 client visible
-        c.border_width = 0
-    elseif #awful.screen.focused().clients > 1 then
-        c.border_width = beautiful.border_width
-        c.border_color = beautiful.border_focus
-	else
-		c.border_width = 0
-    end
+if c.maximized then -- no borders if only 1 client visible
+c.border_width = 0
+elseif #awful.screen.focused().clients > 1 then
+c.border_width = beautiful.border_width
+c.border_color = beautiful.border_focus
+else
+c.border_width = 0
+end
 end
 
 client.connect_signal("property::maximized", border_adjust)
 client.connect_signal("focus", border_adjust)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
-for _,i in pairs(autostart) do
-	awful.spawn.with_shell("~/.config/awesome/autorun.sh " .. i)
+for _, i in pairs(autostart) do
+awful.spawn.with_shell("~/.config/awesome/autorun.sh " .. i)
 end
 
 quake = lain.util.quake {
-	app = terminal,
-	horiz = "center",
-	height = 0.4,
-	width = 1
+app = terminal,
+horiz = "center",
+height = 0.4,
+width = 1
 }
 
 awful.spawn.with_shell("setxkbmap " .. kb_layout)
