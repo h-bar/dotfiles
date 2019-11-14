92cb06d26340ffdaa5e65003f013f934e4d43347
Yan Zhang <663yan@gmail.com>
2019-11-14 06:16:14 +0000

    start some install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+cp -ir  .config     ~
+cp -ir  .scripts    ~
+cp -i   .Xdefaults  ~
+cp -i   .xinitrc    ~
+cp -i   .zshrc      ~
+
+cp ~/.config/awesome/variables.lua.template ~/.config/awesome/variables.lua
---
+++ update.sh
@@ -0,0 +1,5 @@
+cp -ir  .config     ~
+cp -ir  .scripts    ~
+cp -i   .Xdefaults  ~
+cp -i   .xinitrc    ~
+cp -i   .zshrc      ~
