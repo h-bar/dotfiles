dec9b46cbd9c196b411621545882a5fabba1ac68
Yan Zhang <663yan@gmail.com>
2019-11-15 02:04:30 +0000

    install apt packages

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt install rofi fonts-roboto upower acpid pulseaudio libpulse0 pulsemixer mpd mopidy mpc nitrogen i3lock fonts-noto-cjk fonts-noto-cjk-extra fonts-noto-color-emoji fonts-powerline fonts-font-awesome feh rxvt-unicode-256color zsh neovim ncmpcpp ranger pcmanfm fcitx fcitx-googlepinyin --no-install-recommends
+
 ./update.sh
 
 cp ~/.config/awesome/variables.lua.template ~/.config/awesome/variables.lua
