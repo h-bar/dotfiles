0f51c79b6c8cdd0448caf1f762fcbcaf32f20a0f
Yan Zhang <Yan Zhang>
2020-03-23 22:05:37 +0000

    Move zsh to the end

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@ sudo apt update
 
 
 
-sudo apt install -y --no-install-recommends zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 sudo apt install -y --no-install-recommends ranger vim
 
-sudo apt install -y --no-install-recommends xsever-xorg xinit
-sudo apt install -y xmonad
+sudo apt install -y xserver-xorg xinit xmonad
 sudo apt install -y --no-install-recommends fonts-roboto fonts-noto-cjk fonts-noto-cjk-extra fonts-noto-color-emoji fonts-powerline fonts-font-awesome
 sudo apt install -y --no-install-recommends pkg-config libx11-dev libglib2.0-dev libxcomposite-dev
 
@@ -18,3 +14,8 @@ sudo apt install -y --no-install-recommends alacritty xclip
 sudo apt install  -y --no-install-recommends compton feh mpd mpc mopidy rofi upower acpid pulseaudio libpulse0 pulsemixer nitrogen ncmpcpp pcmanfm
 
 sudo apt install -y --no-install-recommends fcitx fcitx-googlepinyin
+
+sudo apt install -y --no-install-recommends zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
